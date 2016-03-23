4e4c7de727291bca34446121451412a6e37a7425
wlchair <wulei0114@126.com>
2016-03-23 15:51:08 +0800

    test successfully

--- test/coveralls_test.js
+++ test/coveralls_test.js
@@ -5,7 +5,6 @@ var fs = require('fs');
 var grunt = require('grunt');
 var sinon = require('sinon');
 
-coveralls.wear!
 
 function runGruntTask(taskName, callback) {
     var task = grunt.task._taskPlusArgs(taskName);
