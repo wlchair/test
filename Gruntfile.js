9a15d15467e3e39b8609601a3486bef917bef164
wlchair <wulei0114@126.com>
2016-07-18 20:47:29 +0800

    test auto

---
+++ Gruntfile.js
@@ -0,0 +1,33 @@
+
+module.exports = function(grunt) {
+    'use strict';
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+	conventionalChangelog: {
+    options: {
+      changelogOpts: {
+        // conventional-changelog options go here
+        preset: 'angular'
+      },
+      context: {
+        // context goes here
+      },
+      gitRawCommitsOpts: {
+        // git-raw-commits options go here
+      },
+      parserOpts: {
+        // conventional-commits-parser options go here
+      },
+      writerOpts: {
+        // conventional-changelog-writer options go here
+      }
+    },
+    release: {
+      src: 'CHANGELOG.md'
+    }
+  }
+});
+
+grunt.loadNpmTasks('grunt-conventional-changelog');
+grunt.registerTask('default', ['conventionalChangelog']);
+};
